3dc99ad243e9dc9399562b5a4f12a580b4e70003
kutluhanyegit <kutluhanyegit@icloud.com>
2022-10-08 10:18:33 +0300

    Autofac Configuration

---
+++ Api/Program.cs
@@ -0,0 +1,48 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Business.Abstract;
+using Business.Concrete;
+using Business.DependencyResolves.Autofac;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EF;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+
+//IOC
+// builder.Services.AddSingleton<IOrderService,OrderManager>();
+// builder.Services.AddSingleton<IOrderDal,EfOrderDal>();
+//dotnet add package Autofac.Extensions.DependencyInjection --version 8.0.0
+
+builder.Host.UseServiceProviderFactory( new AutofacServiceProviderFactory());
+builder.Host.ConfigureContainer<ContainerBuilder>( builder => {
+    builder.RegisterModule( new AutofacBusinessModule());
+});
+
+
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
